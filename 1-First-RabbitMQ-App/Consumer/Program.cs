9fea2b9e0d81f9c4d083b06fb5ffb4c338455451
Brian Delaney <delaneybrian6@gmail.com>
2021-11-03 19:12:07 +0000

    initial commit

---
+++ 1-First-RabbitMQ-App/Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Recieved new message: {message}");
+};
+
+channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
+
+Console.WriteLine("Consuming");
+
+Console.ReadKey();
---
+++ 1-First-RabbitMQ-App/Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = "This is my first Message";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("", "letterbox", null, body);
+
+Console.WriteLine($"Send message: {message}");
